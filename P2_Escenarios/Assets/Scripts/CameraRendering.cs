19b2d58be8451b1fb4abb3d78cea8a63e4b71667
Jony635 <jony635molina@gmail.com>
2018-05-19 11:54:56 +0200

    :)

---
+++ P2_Escenarios/Assets/Scripts/CameraRendering.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+
+public class CameraRendering : MonoBehaviour {
+
+	private Camera playercam;
+	// Use this for initialization
+	void Start () 
+	{
+		playercam = this.gameObject.GetComponent<Camera>();
+		float[] distances = new float[32];
+		distances [8] = 1000;
+		playercam.layerCullDistances = distances;
+	}
+}
