0564687323ca5230d30b95601ac3462d1e86d683
Oriol de Dios <orioldedios@gmail.com>
2018-05-20 21:58:00 +0200

    no toqueis una mierda hijosdeputa

---
+++ P2_Escenarios/Assets/Scripts/animation_exp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation_exp : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision player_col)
+	{
+		if(player_col.gameObject.CompareTag("Player"))
+        {
+            anim.Play("explosion");
+        }
+	}
+
+}
